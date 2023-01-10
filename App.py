3bb6935dc332dd707ba0b22407c37d07e07d9468
emilio barrera <barrera_emilio@yahoo.es>
2023-01-10 22:57:53 +0000

    first commit

---
+++ App.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, url_for, flash
+from werkzeug.utils import redirect
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__)
+app.secret_key = 'many random bytes'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'crud'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def Index():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM students")
+    data = cur.fetchall()
+    cur.close()
+
+    return render_template('index.html', students=data)
+
+if __name__ == "__main__":
+	app.run(port =5555, debug=True)
